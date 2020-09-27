057f4855282db249b36f70cc33e41aa8cff212a3
Federico Sánchez <fedegsancheza@gmail.com>
2020-09-27 19:03:31 -0300

    primer commit

--- myJavascript.js
+++ myJavascript.js
@@ -1,3 +1,3 @@
 function inform(){
-    alert("You have activated me by clicking the grey button! Note that the event handler is added within the event that it handles, in this case, the form button event tag")
+    alert("Hola")
 }
