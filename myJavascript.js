0b7a330ee740775f7c325f2cb5e92e5bebb9aa25
fedegsancheza <63934665+fedegsancheza@users.noreply.github.com>
2022-08-23 19:45:35 -0300

    Update myJavascript.js

--- myJavascript.js
+++ myJavascript.js
@@ -1,3 +1,16 @@
-function inform(){
-    alert("Hola nuevo")
+function inform() {
+    var apigClient = apigClientFactory.newClient();
+    var cuerpo = document.getElementById("query").value;
+    var salida = cuerpo.split('+');
+    let text='{"op1":"'+salida[0]+'","op2":"'+salida[1]+'"}';
+    const obj = JSON.parse(text);
+    apigClient.calcProdHelloPost({},obj,{})
+    .then(function(result){
+        //var resultado=JSON.stringify(result);
+        //console.log(resultado);
+        document.getElementById("query").value= parseInt(result.data.body,10);
+        alert("Holi")
+    }).catch(function(result){
+        alert("ERROR")
+    });
 }
