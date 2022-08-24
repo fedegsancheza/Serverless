c5d23397366f2f313f417da216911e2a0b5e7f96
fedegsancheza <63934665+fedegsancheza@users.noreply.github.com>
2022-08-24 20:17:36 -0300

    Update firstTest.js

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,14 +1,14 @@
 module.exports = {
     'Prueba Suma Calculadora'(browser) {
         const mainQueryInputSelector = 'input[id="query"]';
-	const mainQueryButton = 'name="test"';
+	const mainQueryButton = 'input[id="test"]';
 	const mainQuery = '2+2'
 	var firstPageUrl = "";
         browser
 		.url(function(result) {
   						// return the current url
   						firstPageUrl = result.value;
-  						module.exports.firstPageUrl;
+  						//module.exports.firstPageUrl;
 		});
 		browser.setValue(mainQueryInputSelector,mainQuery)
 	    	browser.click(mainQueryButton)
