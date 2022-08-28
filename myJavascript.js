bd64649cf7113e5f5fee3622bfb26b20868a9729
fedegsancheza <63934665+fedegsancheza@users.noreply.github.com>
2022-08-28 15:27:41 -0300

    Update myJavascript.js

--- myJavascript.js
+++ myJavascript.js
@@ -9,7 +9,7 @@ function inform() {
         //var resultado=JSON.stringify(result);
         //console.log(resultado);
         document.getElementById("query").value= parseInt(result.data.body,10);
-        alert("Holi")
+        //alert("Holi")
     }).catch(function(result){
         alert("ERROR")
     });
