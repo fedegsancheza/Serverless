2761676110e4b1946020c6f5bd3a75d10ce5e72d
fedegsancheza <63934665+fedegsancheza@users.noreply.github.com>
2022-08-28 10:38:48 -0300

    Update firstTest.js

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,16 +1,17 @@
 module.exports = {
     'Prueba Suma Calculadora'(browser) {
-        //const mainQueryInputSelector = 'input[name="query"]';
+       const mainQueryInputSelector = 'input[name="query"]';
 	//const mainQueryInputSelector = 'input[name="test"]';
-	const mainQueryInputSelector = element({
-		selector: 'input[name="query"]',
-		index: 0
-	});
+	//const mainQueryInputSelector = element({
+	//	selector: 'input[name="query"]',
+	//	index: 0
+	//});
 	const mainQueryButton = 'input[name="test"]';
 	const mainQuery = '2+2'
 	var firstPageUrl = "";
         browser
-		.url('https://serverless-unsj.s3.sa-east-1.amazonaws.com/index.html/');
+		//.url('https://serverless-unsj.s3.sa-east-1.amazonaws.com/index.html/');
+		.url('http://localhost:8000/index.html');
 		//.url('https://google.com');
 		//.url(function(result) {
   						// return the current url
@@ -19,7 +20,7 @@ module.exports = {
   						//firstPageUrl = result.value;
   						//module.exports.firstPageUrl;
 		//});
-	    	browser.saveScreenshot('./tests/archivo.png')
+	    	//browser.saveScreenshot('./tests/archivo.png')
 	    	//browser.waitForElementPresent('button[name="test"]',20000)
 		browser.setValue(mainQueryInputSelector,mainQuery)
 	    	browser.click(mainQueryButton)
